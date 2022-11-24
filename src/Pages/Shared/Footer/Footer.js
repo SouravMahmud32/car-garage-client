849184203d2585042bb90f52c1699283a8b9bcb2
sourav-mahmud <souravmahmud32@gmail.com>
2022-11-24 21:28:30 +0600

    footer added

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React from "react";
+import { FaTruckMonster } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
-    return (
+  return (
+    <div>
+      <footer className="footer p-10 mt-32 bg-base-200 text-base-content">
         <div>
-            <h2>This is footer</h2>
+          <Link to="/" className="btn btn-ghost normal-case text-xl">
+            <FaTruckMonster></FaTruckMonster> carGarage
+          </Link>
+          <p>Copyright © 2022 - All right reserved</p>
         </div>
-    );
+        <div>
+          <span className="footer-title">Services</span>
+          <Link to='/' className="link link-hover">Branding</Link>
+          <Link to='/' className="link link-hover">Design</Link>
+          <Link to='/' className="link link-hover">Marketing</Link>
+          <Link to='/' className="link link-hover">Advertisement</Link>
+        </div>
+        <div>
+          <span className="footer-title">Company</span>
+          <Link to='/' className="link link-hover">About us</Link>
+          <Link to='/' className="link link-hover">Contact</Link>
+          <Link to='/' className="link link-hover">Jobs</Link>
+          <Link to='/' className="link link-hover">Press kit</Link>
+        </div>
+        <div>
+          <span className="footer-title">Legal</span>
+          <Link to='/' className="link link-hover">Terms of use</Link>
+          <Link to='/' className="link link-hover">Privacy policy</Link>
+          <Link to='/' className="link link-hover">Cookie policy</Link>
+        </div>
+      </footer>
+    </div>
+  );
 };
 
 export default Footer;
